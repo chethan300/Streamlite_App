f9d1c1bfb46f614ac0a83b9ae3a2f7a729c5ab41
Chethan Kumar K <104550030+chethan300@users.noreply.github.com>
2022-04-28 13:42:48 +0530

    Update Streamlite.py

--- Streamlite.py
+++ Streamlite.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
 streamlit.title("My Parents Healthier Dinner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Mutton Pulav")
 streamlit.text("Chicken Kabab")
 streamlit.text("Chicken Tandori")
 streamlit.header("Build Your Own Fruits Smoothie")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
