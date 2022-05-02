969ca1a9c2fde8ac71fb60e53ff7d6c07ee4b331
Chethan Kumar K <104550030+chethan300@users.noreply.github.com>
2022-05-02 12:43:18 +0530

    Update Streamlite.py

--- Streamlite.py
+++ Streamlite.py
@@ -17,5 +17,10 @@ streamlit.dataframe(fruits_to_show)
 
 ## New section to add fruit vice
 
-fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+##streamlit.text(fruityvice_response.json())
+
+#Normailze the JSOn
+fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
