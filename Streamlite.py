877d15d72d6df6ae8ea2a1680c8bbcccf15f31f0
Chethan Kumar K <104550030+chethan300@users.noreply.github.com>
2022-05-24 22:55:22 +0530

    Update Streamlite.py

--- Streamlite.py
+++ Streamlite.py
@@ -20,16 +20,20 @@ streamlit.dataframe(fruits_to_show)
 
 ## New section to add fruit vice
 streamlit.header("FruityVice Fruit Advisory")
-fruit_choice =streamlit.text_input("What fruit would you like information about ?",'kiwi')
-streamlit.write("the user entered ",fruit_choice )
+try:
+fruit_choice =streamlit.text_input("What fruit would you like information about ?")
+if not fruit_choice :
+  streamlit.error("please select a fruit to get information")
+else:
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 ##streamlit.text(fruityvice_response.json())
-
 #Normailze the JSOn
 fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())
-
 streamlit.dataframe(fruityvice_normalized)
+Except URlError as e:
+  stremlit.error()
+
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
