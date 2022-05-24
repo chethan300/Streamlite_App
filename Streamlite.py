3a87a1a7119df5a26b1c0912f121a7cb20454541
Chethan Kumar K <104550030+chethan300@users.noreply.github.com>
2022-05-24 22:49:23 +0530

    Update Streamlite.py

--- Streamlite.py
+++ Streamlite.py
@@ -1,6 +1,6 @@
 import streamlit
-#import pandas
-#import requests
+import pandas
+import requests
 import snowflake.connector
 from urllib.error  import URLError
 streamlit.title("My Parents Healthier Dinner")
@@ -10,6 +10,7 @@ streamlit.text("Chicken Kabab")
 streamlit.text("Chicken Tandori")
 streamlit.header("Build Your Own Fruits Smoothie")
 
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list= my_fruit_list.set_index('Fruit')
 #lets put a picklist so that they can pick the fruit they want to include
@@ -21,6 +22,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("FruityVice Fruit Advisory")
 fruit_choice =streamlit.text_input("What fruit would you like information about ?",'kiwi')
 streamlit.write("the user entered ",fruit_choice )
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 ##streamlit.text(fruityvice_response.json())
 
