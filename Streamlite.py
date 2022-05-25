74d1254f78dcc1dd1c462790830080c0fa40ba3b
Chethan Kumar K <104550030+chethan300@users.noreply.github.com>
2022-05-25 11:10:36 +0530

    Update Streamlite.py

--- Streamlite.py
+++ Streamlite.py
@@ -25,13 +25,14 @@ fruit_choice=streamlit.text_input('What fruit would you like information about ?
 if not fruit_choice:
   streamlit.error("please select a fruit to get information")
 else:
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-##streamlit.text(fruityvice_response.json())
-#Normailze the JSOn
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 fruityvice_normalized =pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-Except URlError as e:
+##streamlit.text(fruityvice_response.json())
+#Normailze the JSOn
+
+Except URLError as e:
+  
   streamlit.error()
 
 streamlit.stop()
